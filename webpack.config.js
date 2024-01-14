e5eda0d8a67a2667583b44558cc5ea5654462089
Ahmed Wael <ahmedwael216@protonmail.com>
2024-01-14 11:33:46 +0200

    addded image loader

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,18 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+	  {
+		test: /\.(png|svg|jpg|gif|jpe?g)$/,
+		use: [
+		  {
+			options: {
+			  name: "[name].[ext]",
+			  outputPath: "images/"
+			},
+			loader: "file-loader"
+		  }
+		]
+	  }
     ],
   },
 };
